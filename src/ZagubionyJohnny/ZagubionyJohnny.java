d0d5bcb26e11b2467ba05305bb4b3c2650c84b34
wasilczyk-pawel <wasilczyk-pawel@users.noreply.github.com>
2015-11-24 16:59:01 +0100

    Nowa gra z kratą

--- src/ZagubionyJohnny/ZagubionyJohnny.java
+++ src/ZagubionyJohnny/ZagubionyJohnny.java
@@ -3,14 +3,19 @@ package ZagubionyJohnny;
 import java.awt.Color;
 import java.awt.Toolkit;
 import java.awt.Font;
-
+import java.awt.GridLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.JPanel;
 
 public class ZagubionyJohnny extends JFrame
-{	
+{
+       private static final int fieldWidth=5;
+       private static final int fieldHeight=5;
+       private final Room[][] fieldButtons;
+        
 	public ZagubionyJohnny() 
 	{
 		setIconImage(Toolkit.getDefaultToolkit().getImage("ikonka.png"));
@@ -34,6 +39,21 @@ public class ZagubionyJohnny extends JFrame
 		konsola.setBounds(10, 510, 780, 300);
 		konsola.setLineWrap(true);
 		getContentPane().add(konsola);
+		
+               JPanel field = new JPanel();
+               field.setBounds(525, 10, fieldWidth*50, fieldHeight*50);
+               field.setLayout(new GridLayout(fieldHeight, fieldWidth, 0, 0));
+               fieldButtons=new Room[fieldWidth][fieldHeight];
+               for (int j=0; j<fieldHeight; j++)
+               {    
+                   for (int i=0; i<fieldWidth; i++)
+                   {
+                       fieldButtons[i][j] = new Room(i,j);
+                       fieldButtons[i][j].setBackground(Color.gray);
+                       field.add(fieldButtons[i][j]);
+                   }
+               }
+               getContentPane().add(field);
 	}
 
 	public static void main(String[] args) 
